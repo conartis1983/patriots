58d0c39d3dd80215971ac30fd31a7d78963070a2
conartis1983 <conartis1983@gmail.com>
2025-06-23 11:12:57 +0200

    Customization of the Desing

--- resources/views/components/secondary-button.blade.php
+++ resources/views/components/secondary-button.blade.php
@@ -1,3 +1,6 @@
-<button {{ $attributes->merge(['type' => 'button', 'class' => 'inline-flex items-center px-4 py-2 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-500 rounded-md font-semibold text-xs text-gray-700 dark:text-gray-300 uppercase tracking-widest shadow-sm hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 disabled:opacity-25 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge([
+    'type' => 'button',
+    'class' => 'inline-flex items-center px-2.5 py-1.5 bg-neutral-200 border border-neutral-300 text-xs font-bold uppercase tracking-wide rounded shadow-sm hover:bg-neutral-300 focus:outline-none focus:ring-2 focus:ring-neutral-400 focus:ring-offset-2 transition disabled:opacity-50 disabled:cursor-not-allowed'
+]) }}>
     {{ $slot }}
 </button>
